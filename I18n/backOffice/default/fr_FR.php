f02caabd14999e731be9787cd3fa7e594d67baaf
NicolasBarbey <223106@supinfo.com>
2019-09-24 12:40:44 +0200

    Update fr_FR.php

--- I18n/backOffice/default/fr_FR.php
+++ I18n/backOffice/default/fr_FR.php
@@ -11,7 +11,7 @@ return array(
     'Import translations' => 'Importer les traductions',
     'Mail template you want to export' => 'Template d\'e-mail que vous voulez exporter',
     'PDF template you want to export' => 'Template PDF que vous voulez exporter',
-    'Select extension' => 'Sélectionner une extention',
+    'Select extension' => 'Sélectionner une extension',
     'Translations' => 'Traductions',
     'Zip the folder you have exported before with exactly the same structure and with your translation modification. And upload it here.' => 'Compressez le dossier que vous avez exporté avec exactement la même structure et avec la modification de vos traduction. Et envoyez-le ici.',
 );
